6c0a5e562f69e08e6f8a57790496ab9254955198
Maksim Dorogoi <monarch2013@mail.ru>
2023-11-08 20:08:00 +1100

    Styled Components Applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import backgr from "./backgr.jpg";
+
+const Paragraph = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 20px;
+  color: #abb3ba;
+  text-align: center;
+`;
+
+const HeaderH1 = styled.h1`
+text-align: center;
+`
+
+const StyledBtnL = styled.button`
+  border: none;
+  background-color: hsl(147, 50%, 47%);
+  color: snow;
+  padding: 10px 20px;
+  font-size: 10px;
+  font-weight: 700;
+  line-height: 20px;
+  margin-right: 10px;
+  border-radius: 5px;
+  margin-left: 20px;
+  font-family: Inter;
+
+  &: hover {
+    background-color: #4e71fe;
+  }
+`;
+
+const StyledBtnR = styled(StyledBtnL)`
+  background-color: yellow;
+  color: #4e71fe;
+  padding: 10px 38px 10px 38px;
+
+  &: hover {
+    background-color: transparent;
+    border: 2px solid #4e71fe;
+  }
+`;
+const BtnContainer = styled.div`
+  display: block;
+ text-align: center;
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+const CenterImg = styled.img`
+display: block;
+margin: auto;
+width: 50vw;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CenterImg src={backgr} alt=""/>
+      <HeaderH1>Headline</HeaderH1>
+      <Paragraph>
+        Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie
+        ornare in venen.
+      </Paragraph>
+      <BtnContainer>
+        <StyledBtnL>See more</StyledBtnL>
+        <StyledBtnR>Save</StyledBtnR>
+      </BtnContainer>
     </div>
   );
 }
