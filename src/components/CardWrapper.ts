da464704ed4f1cad7dfe4aec428fd47df61cd2d9
Maksim Dorogoi <monarch2013@mail.ru>
2024-01-04 14:57:52 +1100

    props

--- src/components/CardWrapper.ts
+++ src/components/CardWrapper.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export const CardWrapper = styled.div`
+export const CardWrapper = styled.div<{color?: string}>`
   padding: 10px 10px 25px 10px;
   border-radius: 10px;
-  background-color: rgb(255, 255, 255);
+  background-color: ${props => props.color || '#FFFFFF'};
   box-shadow: 0px 4px 20px 5px #0000001a;
 `;
