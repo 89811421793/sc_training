e4ba2e7176882f78dd86912bce0e332a601256c3
Maksim Dorogoi <monarch2013@mail.ru>
2024-01-04 15:58:02 +1100

    propstype

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
-const HeaderH1 = styled.h1`
+type  HeaderH1PropsType = {
+  fontSize?: string
+}
+
+const HeaderH1 = styled.h1<HeaderH1PropsType>`
   font-weight: 700;
-  font-size: 16px;
+  font-size: ${props => props.fontSize || '16px'};
   line-height: 19.36px;
   margin-bottom: 20px;
   margin-left: 8px;
